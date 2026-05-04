a77c509a92146a62e03f7b9795cbc2c9361f1e5b
parthshah2111work-hash <parthshah2111work@gmail.com>
2026-05-04 13:23:29 +0000

    11th Step -- Counter Gradle 105

--- withKotlinFix.js
+++ withKotlinFix.js
@@ -1,23 +1,38 @@
-const { withProjectBuildGradle } = require('@expo/config-plugins');
+const {
+  withProjectBuildGradle,
+  withAppBuildGradle,
+} = require("@expo/config-plugins");
 
 module.exports = (config) => {
-  return withProjectBuildGradle(config, (config) => {
-    if (config.modResults.language === 'groovy') {
+  // Fix 1: Root build.gradle (Kotlin Version)
+  config = withProjectBuildGradle(config, (config) => {
+    if (config.modResults.language === "groovy") {
       let content = config.modResults.contents;
-
-      // Force Kotlin 2.1.20 at the absolute start of buildscript
       if (!content.includes("kotlinVersion = '2.1.20'")) {
         content = content.replace(
           /buildscript\s*{/,
-          "buildscript {\n    ext.kotlinVersion = '2.1.20'"
+          "buildscript {\n    ext.kotlinVersion = '2.1.20'",
         );
       }
-
-      // Wipe out any old 1.9.24 references that cause KSP errors
       content = content.replace(/1\.9\.24/g, "2.1.20");
-
       config.modResults.contents = content;
     }
     return config;
   });
+
+  // Fix 2: App build.gradle (Remove obsolete enableBundleCompression)
+  config = withAppBuildGradle(config, (config) => {
+    if (config.modResults.language === "groovy") {
+      let content = config.modResults.contents;
+      // Remove the line causing the Line 98 error
+      content = content.replace(
+        /enableBundleCompression\s*=\s*(true|false)/g,
+        "",
+      );
+      config.modResults.contents = content;
+    }
+    return config;
+  });
+
+  return config;
 };
