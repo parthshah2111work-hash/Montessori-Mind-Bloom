970f5f530681e9208d390574a471e31962425adf
parthshah2111work-hash <parthshah2111work@gmail.com>
2026-05-04 12:27:28 +0000

    10th Step -- to counter Gradelew 92

--- withKotlinFix.js
+++ withKotlinFix.js
@@ -4,13 +4,21 @@ module.exports = (config) => {
   return withProjectBuildGradle(config, (config) => {
     if (config.modResults.language === "groovy") {
       let content = config.modResults.contents;
-      // This is the absolute fail-safe injection
+
+      // Force Kotlin 2.1.20 in the ext block
+      content = content.replace(
+        /kotlinVersion\s*=\s*['"][^'"]*['"]/g,
+        "kotlinVersion = '2.1.20'",
+      );
+
+      // Force all other references to 2.1.20
       if (!content.includes("kotlinVersion =")) {
         content = content.replace(
           /ext\s*{/,
           "ext {\n        kotlinVersion = '2.1.20'",
         );
       }
+
       config.modResults.contents = content;
     }
     return config;
