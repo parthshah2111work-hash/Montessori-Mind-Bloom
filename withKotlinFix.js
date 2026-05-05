b3a5662421937de418e83b4fc8fe567f4edc3a2f
parthshah2111work-hash <parthshah2111work@gmail.com>
2026-05-05 09:34:14 +0000

    Fix syntax error in root build.gradle

--- withKotlinFix.js
+++ withKotlinFix.js
@@ -1,22 +1,28 @@
 const { withProjectBuildGradle, withAppBuildGradle } = require("@expo/config-plugins");
 
 module.exports = (config) => {
+  // 1. ROOT FIX: Remove the risky text replacement and just append the force
   config = withProjectBuildGradle(config, (config) => {
     if (config.modResults.language === "groovy") {
       let content = config.modResults.contents;
-      content = content.replace(/kotlinVersion\s*=\s*['"].*?['"]/g, "kotlinVersion = '2.1.0'");
-      content = content.replace(
-        /classpath\s*['"]org\.jetbrains\.kotlin:kotlin-gradle-plugin:.*?['"]/g,
-        "classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0'"
-      );
+
+      // Add the global kotlinVersion at the very top of buildscript if not present
+      if (!content.includes("kotlinVersion = '2.1.0'")) {
+        content = content.replace(
+          "buildscript {",
+          "buildscript {\n    ext.kotlinVersion = '2.1.0'"
+        );
+      }
       config.modResults.contents = content;
     }
     return config;
   });
 
+  // 2. APP FIX: The "Safe" Purge + Global Force
   config = withAppBuildGradle(config, (config) => {
     if (config.modResults.language === "groovy") {
       let content = config.modResults.contents;
+
       const propertiesToPurge = [
         /enableBundleCompression\s*=\s*.*?\n/g,
         /preloadedNativeModules\s*=\s*.*?\n/g,
@@ -31,7 +37,6 @@ module.exports = (config) => {
         resolutionStrategy {
             force "org.jetbrains.kotlin:kotlin-stdlib:2.1.0"
             force "org.jetbrains.kotlin:kotlin-reflect:2.1.0"
-            force "org.jetbrains.kotlin:kotlin-compiler-embeddable:2.1.0"
             force "com.google.devtools.ksp:symbol-processing-api:2.1.0-1.0.29"
         }
     }
