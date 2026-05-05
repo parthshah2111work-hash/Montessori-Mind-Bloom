ac6d89b427394f9980030a8258417b050a88d044
parthshah2111work-hash <parthshah2111work@gmail.com>
2026-05-05 13:48:32 +0000

    Resolving "Gradle-445 error"

--- withKotlinFix.js
+++ withKotlinFix.js
@@ -1,13 +1,11 @@
 const { withProjectBuildGradle, withAppBuildGradle } = require("@expo/config-plugins");
-const fs = require('fs');
-const path = require('path');
 
 module.exports = (config) => {
   config = withProjectBuildGradle(config, (config) => {
     if (config.modResults.language === "groovy") {
       let content = config.modResults.contents;
-      if (!content.includes("kotlinVersion = '2.0.20'")) {
-        content = content.replace("buildscript {", "buildscript {\n    ext.kotlinVersion = '2.0.20'");
+      if (!content.includes("kotlinVersion = '2.0.0'")) {
+        content = content.replace("buildscript {", "buildscript {\n    ext.kotlinVersion = '2.0.0'");
       }
       config.modResults.contents = content;
     }
@@ -24,18 +22,20 @@ module.exports = (config) => {
         const resolutionFix = `
     configurations.all {
         resolutionStrategy {
-            force "org.jetbrains.kotlin:kotlin-stdlib:2.0.20"
-            force "org.jetbrains.kotlin:kotlin-reflect:2.0.20"
-            force "com.google.devtools.ksp:symbol-processing-api:2.0.20-1.0.24"
+            force "org.jetbrains.kotlin:kotlin-stdlib:2.0.0"
+            force "org.jetbrains.kotlin:kotlin-reflect:2.0.0"
+            force "com.google.devtools.ksp:symbol-processing-api:2.0.0-1.0.21"
         }
     }
     tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile).all {
         kotlinOptions {
+            allWarningsAsErrors = false
             freeCompilerArgs += [
-            "-Xexpect-actual-classes", 
-            "-Xskip-metadata-version-check",
-            "-Xsuppress-version-warnings",
-            "-P", "plugin:org.jetbrains.kotlin.parcelize:additionalAnnotation=expo.modules.kotlin.apidels.Parcelize"
+                "-Xexpect-actual-classes",
+                "-Xskip-metadata-version-check",
+                "-Xsuppress-version-warnings",
+                "-Xno-call-assertions",
+                "-Xno-receiver-assertions"
             ]
         }
     }
